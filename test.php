e83b0536309625c58950c57053a9c213b5982f05
rwifeng <rw.ifeng@gmail.com>
2013-03-19 20:20:27 +0800

    fix

--- test.php
+++ test.php
@@ -1,9 +1,7 @@
 <?php  
 class MyTestClass extends PHPUnit_Framework_TestCase  
 {  
-/**  
-* Testing the answer to Òdo you love unit tests?Ó  
-*/ 
+
 public function testDoYouLoveUnitTests()  
 {  
 $love = true;  
